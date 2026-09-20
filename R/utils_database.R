5480672d131f2d737c0b21f311d5077ff685e10d
Oliver Eaton <eat_fred@protonmail.com>
2026-09-20 14:42:06 +1200

    updated db creds

--- R/utils_database.R
+++ R/utils_database.R
@@ -3,26 +3,26 @@
 #' @importFrom RPostgres Postgres
 db_con <- function() {
   # Cockroach
-  # dbConnect(
-  #   drv = Postgres(),
-  #   user = "kobe_public",
-  #   password = "kobe123-69(.)(.)",
-  #   host = "nba-data-mgmt-9184.8nj.gcp-europe-west1.cockroachlabs.cloud",
-  #   port = "26257",
-  #   dbname = "nba",
-  #   options = "--cluster=nba-data-mgmt-9184"
-  # )
-
-  # Postgres
   dbConnect(
     drv = Postgres(),
-    user = "postgres",
-    password = "zxc123",
-    host = "localhost",
-    port = "5432",
+    user = "kobe_public",
+    password = "kobe123-69(.)(.)",
+    host = "nba-data-mgmt-9184.8nj.gcp-europe-west1.cockroachlabs.cloud",
+    port = "26257",
     dbname = "nba",
-    options = "-c search_path=nba"
+    options = "--cluster=nba-data-mgmt-9184"
   )
+
+  # Postgres
+  # dbConnect(
+  #   drv = Postgres(),
+  #   user = "postgres",
+  #   password = "zxc123",
+  #   host = "localhost",
+  #   port = "5432",
+  #   dbname = "nba",
+  #   options = "-c search_path=nba"
+  # )
 }
 
 
