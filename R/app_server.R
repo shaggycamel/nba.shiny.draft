5352d8f0d4e63d063721a64e425c1c96fa227f8a
Oliver Eaton <eat_fred@protonmail.com>
2026-09-22 11:56:39 +1200

    update

--- R/app_server.R
+++ R/app_server.R
@@ -3,6 +3,7 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @importFrom DBI dbDisconnect
 #' @noRd
 app_server <- function(input, output, session) {
   #
