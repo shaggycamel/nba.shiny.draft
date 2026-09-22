2555d728cc2d6a9d9f83d2ea130175f01f06f571
Oliver Eaton <eat_fred@protonmail.com>
2026-09-22 13:15:30 +1200

    upto HF secrets

--- R/utils_database.R
+++ R/utils_database.R
@@ -1,28 +1,65 @@
-# Read only cockroach database connection\
+# Read only cockroach database connection
+#' @importFrom ini read.ini
 #' @importFrom DBI dbConnect
 #' @importFrom RPostgres Postgres
-db_con <- function() {
-  # Cockroach
-  dbConnect(
+db_con <- function(source = Sys.getenv("NBA_DB_SOURCE", "cockroach")) {
+  source <- match.arg(source, c("cockroach", "postgres"))
+  cfg <- db_config(source)
+
+  args <- list(
     drv = Postgres(),
-    user = "kobe_public",
-    password = "kobe123-69(.)(.)",
-    host = "nba-data-mgmt-9184.8nj.gcp-europe-west1.cockroachlabs.cloud",
-    port = "26257",
-    dbname = "nba",
-    options = "--cluster=nba-data-mgmt-9184"
+    user = cfg$user,
+    password = cfg$password,
+    host = cfg$host,
+    port = cfg$port,
+    dbname = cfg$dbname
+  )
+  if (!is.null(cfg$options)) {
+    args$options <- cfg$options
+  }
+
+  do.call(dbConnect, args)
+}
+
+
+#' @noRd
+db_config <- function(source) {
+  if (file.exists("credentials.ini")) {
+    cfg <- ini::read.ini("credentials.ini")[[source]]
+    if (is.null(cfg)) {
+      stop("No [", source, "] section in credentials.ini", call. = FALSE)
+    }
+    return(cfg)
+  }
+
+  prefix <- toupper(source)
+  get_var <- function(key) {
+    val <- Sys.getenv(paste0(prefix, "_", key))
+    if (identical(val, "")) NULL else val
+  }
+
+  cfg <- list(
+    user = get_var("USER"),
+    password = get_var("PASSWORD"),
+    host = get_var("HOST"),
+    port = get_var("PORT"),
+    dbname = get_var("DBNAME"),
+    options = get_var("OPTIONS")
   )
 
-  # Postgres
-  # dbConnect(
-  #   drv = Postgres(),
-  #   user = "postgres",
-  #   password = "zxc123",
-  #   host = "localhost",
-  #   port = "5432",
-  #   dbname = "nba",
-  #   options = "-c search_path=nba"
-  # )
+  required <- c("user", "password", "host", "port", "dbname")
+  missing <- required[vapply(cfg[required], is.null, logical(1))]
+  if (length(missing) > 0) {
+    stop(
+      "Missing env vars for source '",
+      source,
+      "': ",
+      paste0(prefix, "_", toupper(missing), collapse = ", "),
+      call. = FALSE
+    )
+  }
+
+  cfg
 }
 
 
